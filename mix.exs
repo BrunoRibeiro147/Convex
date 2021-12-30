4f4c723243fa3334b8b337ac95e481d46330808a
Bruno <brf147@gmail.com>
2021-12-30 12:24:48 -0400

    feat: Setup CI Pipeline

--- mix.exs
+++ mix.exs
@@ -64,7 +64,7 @@ defmodule Core.MixProject do
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "run priv/repo/seeds.exs", "test"]
     ]
   end
 end
