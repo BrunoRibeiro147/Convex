c431220d31e1b3378645c2c1a4aeb63425af15c8
Bruno <brf147@gmail.com>
2022-01-04 09:22:01 -0400

    Fix endpoint for using releases

--- config/releases.exs
+++ config/releases.exs
@@ -25,7 +25,7 @@ config :convex, WebAPI.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :convex, WebAPI.Endpoint, server: true
+config :convex, WebAPI.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
